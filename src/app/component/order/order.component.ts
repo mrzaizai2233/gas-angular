07c41d14b5745cb6461cf6640e65b3c2d5ae17f2
mrzaizai2233 <kiepsongthua18@gmail.com>
2017-12-09 17:26:10 +0700

    order form

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
-  constructor() { }
+  orderForm:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.fb.group({
+      _id:[],
+      user:this.fb.group({
+        name:'',
+        code:'',
+        adress:'',
+        phone:''
+      }),
+      grand_total:[],
+      subtotal:[],
+      items:this.fb.array([])
+    })
+  }
+  get items(){
+    return this.orderForm.get('items') as FormArray;
+  }
+  addItem(){
+   
+    this.items.push(this.fb.group({
+      product:'',
+      qty:'',
+      price:'',
+      discout:'',
+      discout_total:'',
+      note:'',
+    }))
+  }
+  removeItem(index){
+    console.log(index)
+    for(let i=0;i<this.items.controls.length;i++){
+      // console.log(this.items.controls[i])
+      if(i==index){
+          this.items.removeAt(index)
+      }
+    }
+  }
+
+  onSubmit(){
+
   }
 
 }
